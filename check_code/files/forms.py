4e14dde9d8ceb36ad1ef1a3102cf624686b0a1fb
Sergey Afonin <afoninsb@yandex.ru>
2023-08-18 09:12:19 +0300

    validate .py

--- check_code/files/forms.py
+++ check_code/files/forms.py
@@ -7,3 +7,10 @@ class CodeFileForm(forms.ModelForm):
     class Meta:
         model = CodeFile
         fields = ('upload', 'description')
+
+    def clean_upload(self):
+        data = self.cleaned_data['upload']
+        ext = data.name.split('.')[-1]
+        if ext != 'py':
+            raise forms.ValidationError('Можно только .py файлы')
+        return data
